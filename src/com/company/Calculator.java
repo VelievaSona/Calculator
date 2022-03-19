3a996a94131cafff5a30dc1884a59519baf1adaf
Sona <sonochka.velieva@yandex.ru>
2022-03-19 21:49:26 +0400

    Initial commit

---
+++ src/com/company/Calculator.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.security.InvalidParameterException;
+
+public class Calculator {
+
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        /**
+         * Ошибка деления на ноль. Обработка этой ошибка указана ниже
+         */
+        if (y == 0){
+            throw new InvalidParameterException("Деление на 0");
+        }
+
+        return x / y;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
